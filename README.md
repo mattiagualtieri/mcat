08414765dc9ad2eea2708cc8d148c98df41eb3a1
Mattia Gualtieri <mattiagualtieri2000@gmail.com>
2024-03-08 19:12:42 +0100

    Some basic refactoring

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MCAT
 An MCAT (Multimodal Co-Attention Transformer) implementation for overall survival regression using WSI and omics data
+
+## Architecture
+<img src="docs/architecture.jpeg" width="1500px" align="center" />
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import yaml
 import torch
 
+from omics.preprocessing import preprocess_omics
+
 
 def main():
+    with open('config/config.yaml') as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f'Running on {device}')
+
+    omics_raw_input = config['omics_raw_input']
+    omics_signatures = config['omics_signatures']
+    omics_processed_output_dir = config['omics_processed_output_dir']
+    preprocess_omics(omics_raw_input, omics_signatures, omics_processed_output_dir)
+    omics_input_dir = omics_processed_output_dir
 
 
 if __name__ == '__main__':
