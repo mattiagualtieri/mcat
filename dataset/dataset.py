b09c6d413f3b3a05a38ca7595ee5c0752b981a4a
Mattia Gualtieri <mattiagualtieri2000@gmail.com>
2024-03-22 21:34:42 +0100

    Added WSI preprocessing

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -14,11 +14,14 @@ class MultimodalDataset(Dataset):
         case_id = self.case_ids[index]
         case_group = self.hdf5_file[case_id]
 
-        omics_group = case_group['omics']
-        omics_data = [torch.tensor(omics_group[dataset][:]) for dataset in omics_group]
-
         labels_group = case_group['labels']
         overall_survival = torch.tensor(labels_group['overall_survival'][()])
         survival_risk = torch.tensor(labels_group['survival_risk'][()])
 
-        return omics_data, overall_survival, survival_risk
+        omics_group = case_group['omics']
+        omics_data = [torch.tensor(omics_group[dataset][:]) for dataset in omics_group]
+
+        wsi_group = case_group['wsi']
+        patches_embeddings = torch.tensor(wsi_group['patches'][()])
+
+        return overall_survival, survival_risk, omics_data, patches_embeddings
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from torch.utils.data import DataLoader
 
 from labels.preprocessing import preprocess_labels
 from omics.preprocessing import preprocess_omics
+from wsi.preprocessing import preprocess_patch_embeddings
 from dataset.dataset import MultimodalDataset
 
 
@@ -17,11 +18,12 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f'Running on {device}')
 
-    raw_input = config['raw_input']
     dataset_file = config['dataset_file']
     if os.path.exists(dataset_file):
         print(f'Skipping {dataset_file} creation: already exists')
     else:
+        raw_input = config['raw_input']
+
         preprocess_labels(raw_input, dataset_file)
         print(f'Created dataset {dataset_file}')
 
@@ -33,15 +35,18 @@ def main():
         # - take each WSI slide
         # - divide it into M patches (256x256)
         # - embed each patch with a ResNet50 (pretrained), so we obtain a Mx1024 matrix
-        # - this matrix is created into a .pt file, that we must read and add to the dataset
         print('Note: skipping all patches creation and embedding part...')
+        # - The matrix is created into a .pt file, that we must read and add to the dataset
+        patch_emb_dir = config['patch_emb_dir']
+        preprocess_patch_embeddings(patch_emb_dir, dataset_file)
+        print(f'Added patch embeddings to dataset {dataset_file}')
 
     with h5py.File(dataset_file, 'r') as hdf5_file:
         dataset = MultimodalDataset(hdf5_file)
         dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
 
         # Iterate over batches of data from your dataset
-        for batch_index, (omics_data, overall_survival, survival_risk) in enumerate(dataloader):
+        for batch_index, (overall_survival, survival_risk, omics_data, patches_embeddings) in enumerate(dataloader):
             # Process the data as needed
             # print("Omics Data:", omics_data)
             print("Overall Survival:", overall_survival)
